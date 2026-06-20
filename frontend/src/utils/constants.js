01a8eb18db671487928d6cbe5062c95d6dbace86
Hardik585 <Hardikkaushik83126@gmail.com>
2026-06-20 15:55:01 +0530

    env setup for frontend hosting

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,5 +3,5 @@ export const AppConstants = {
   APP_NAME: '',
   // API_BASE_URL: 'http://54.196.208.202:8080/api/v1.0',
   API_BASE_URL_LOCAL: 'http://localhost:8080/api/v1.0',  
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL
+  API_BASE_URL: import.meta.env.VITE_API_URL
 }
